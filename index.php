1f71879441e11327291c5fb6ef490ab8e93ddd18
AlbertoDiLeo <115349758+AlbertoDiLeo@users.noreply.github.com>
2025-03-03 09:20:40 +0100

    Aggiungi sezioni "Chi Sono" e "Di cosa mi occupo" in index.php, rimuovi codice commentato e semplifica la logica della mappa in script.js. Aggiorna lo stile dei bottoni personalizzati in style.css.

--- index.php
+++ index.php
@@ -1,104 +1,3 @@
-<!-- <!DOCTYPE html>
-<html lang="it">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Home | Lorenzo Lunghi</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="style.css">
-  <link rel="icon" href="immagini/Logo foto.JPG" type="image/x-icon">
-</head>
-<body onload="initMap()">
-    <div class="main-content">
-        <div class="container text-center mt-3">
-            <h1>Geometra Lunghi Lorenzo</h1>
-        </div>
-
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav mx-auto">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="index.php">Homeee</a>
-                          </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="chi-sono.php">Chi sono</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="servizi.php">Serviziii</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="contatti.php">Contatti</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        
-        <div class="container text-center mt-5">
-            <img src="immagini/Logo foto.JPG" class="img-fluid logo-img"  alt="Benvenuto">
-        </div>
-         
-        <div class="container text-center mt-5">
-            <div class="row">
-                <div class="col-12">
-                    <h2>Chiamaci ora</h2>
-                    <p>Informazioni sulla nostra azienda e il nostro team</p>
-                    <a href="tel:+393343693310" class="btn btn-primary btn-lg mt-3">Chiama ora</a>
-                </div>
-            </div>
-        </div>
-
-
-        <div class="container text-center mt-5">
-            <div class="row">
-                <div class="col-12">
-                    <h2>Dove sono</h2>
-                    <a href="https://maps.app.goo.gl/AydxDqMKJPuxdny78" class="address-link"><span>Via Sandro Pertini, 6 - 26817, San Martino in Strada (Lo), Italia</span></a>
-                    <div id="map" style="height: 400px; width: 100%;"></div>
-                </div>
-            </div>
-        </div>
-
-        
-        <footer class="bg-light text-center text-lg-start mt-5">
-         <div class="container p-4">
-                <div class="row">
-                    <div class="col-lg-6 col-md-12 mb-4 mb-md-0 text-center">
-                        <h5 class="text-uppercase">Contatti</h5>
-                        <p>Email: <a href="mailto:Info.geomlunghi@gmail.com">Info.geomlunghi@gmail.com</a></p>
-                        <p>Telefono: +39 334 369 3310</p>
-                    </div>
-                    <div class="col-lg-6 col-md-12 mb-4 mb-md-0 text-center">
-                        <h5 class="text-uppercase">Indirizzo</h5>
-                        <p>Via Sandro Pertini, 6 - 26817, San Martino in Strada (Lo), Italia</p>
-                    </div>
-                </div>
-            </div> 
-            <div class="text-center p-3 bg-dark text-white">
-                <a href="mailto:Info.geomlunghi@gmail.com" class="footer-link">Info.geomlunghi@gmail.com</a>
-                <p><a href="tel:+39 334 369 3310" class="footer-link"><span>Telefono: +39 334 369 3310</span></a></p>
-                <p><a href="https://www.instagram.com/lorenzo.lunghi_/" target="_blank" class="footer-link">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-                        <path fill="none" d="M0 0h24v24H0z" />
-                        <path
-                            d="M12 2c2.717 0 3.056.01 4.122.06 1.065.05 1.79.217 2.428.465.66.254 1.216.598 1.772 1.153a4.908 4.908 0 0 1 1.153 1.772c.247.637.415 1.363.465 2.428.047 1.066.06 1.405.06 4.122 0 2.717-.01 3.056-.06 4.122-.05 1.065-.218 1.79-.465 2.428a4.883 4.883 0 0 1-1.153 1.772 4.915 4.915 0 0 1-1.772 1.153c-.637.247-1.363.415-2.428.465-1.066.047-1.405.06-4.122.06-2.717 0-3.056-.01-4.122-.06-1.065-.05-1.79-.218-2.428-.465a4.89 4.89 0 0 1-1.772-1.153 4.904 4.904 0 0 1-1.153-1.772c-.248-.637-.415-1.363-.465-2.428C2.013 15.056 2 14.717 2 12c0-2.717.01-3.056.06-4.122.05-1.066.217-1.79.465-2.428a4.88 4.88 0 0 1 1.153-1.772A4.897 4.897 0 0 1 5.45 2.525c.638-.248 1.362-.415 2.428-.465C8.944 2.013 9.283 2 12 2zm0 5a5 5 0 1 0 0 10 5 5 0 0 0 0-10zm6.5-.25a1.25 1.25 0 0 0-2.5 0 1.25 1.25 0 0 0 2.5 0zM12 9a3 3 0 1 1 0 6 3 3 0 0 1 0-6z" />
-                    </svg>
-                </a></p>
-              © 2024 Geometra Lunghi Lorenzo. Tutti i diritti riservati.
-            </div>
-        </footer>
-    </div>
-  
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="script.js"></script>
-</body>
-</html> -->
-
 <!DOCTYPE html>
 <html lang="it">
 <head>
@@ -131,12 +30,38 @@
             </div>
         </section>
 
+        <!-- CHI SONO -->
+        <section class="py-5 bg-secondary text-white text-center">
+            <div class="container">
+                <h2 class="fw-bold">Chi Sono</h2>
+                <p class="lead">
+                    Uno studio tecnico con esperienza e passione.<br>
+                    Soddisfo ogni esigenza con soluzioni su misura.
+                </p>
+                <a href="chi-sono.php" class="btn btn-outline-light btn-lg custom-btn-chi-sono" >🧑‍💼 Scopri di più</a>
+            </div>
+        </section>
+
+        <!-- DI COSA MI OCCUPO -->
+        <section class="py-5 bg-light text-center">
+            <div class="container">
+                <h2 class="fw-bold">Di cosa mi occupo</h2>
+                <p class="lead">
+                    Servizi tecnici professionali per il settore edilizio.<br>
+                    Pratiche catastali, progettazione e consulenze di qualità.
+                </p>
+                <a href="servizi.php" class="btn btn-outline-secondary btn-lg btn-lg custom-btn-servizi">🏗️ Scopri i servizi</a>
+            </div>
+        </section>
+
+
+
         <!-- PRONTO INTERVENTO H-24 -->
         <section class="py-5 bg-dark text-white text-center">
             <div class="container">
                 <h2 class="fw-bold">Pronto Intervento H-24</h2>
                 <p class="lead">Hai un’urgenza? Contattami subito, sono sempre operativo per offrirti assistenza immediata.</p>
-                <a href="tel:+393343693310" class="btn btn-outline-warning btn-lg">📞 Chiama Subito</a>
+                <a href="tel:+393343693310" class="btn btn-outline-warning btn-lg custom-btn-intervento">📞 Chiama Subito</a>
             </div>
         </section>
 
@@ -155,15 +80,6 @@
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
-
-        <!-- CHI SIAMO -->
-        <section class="py-5 text-center">
-            <div class="container">
-                <h2 class="fw-bold">Chi Sono</h2>
-                <p class="lead">Uno studio tecnico con esperienza e passione, pronto a soddisfare ogni esigenza con soluzioni su misura.</p>
-                <a href="chi-sono.php" class="btn btn-primary">Scopri di più</a>
             </div>
         </section>
 
--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-function initMap() {
-    var geocoder = new google.maps.Geocoder();
-    var address = 'Via Sandro Pertini, 6, 26817 San Martino in Strada LO, Italia'; // Sostituisci con il tuo indirizzo
-
-    geocoder.geocode({ 'address': address }, function(results, status) {
-      if (status === 'OK') {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 15,
-          center: results[0].geometry.location
-        });
-        var marker = new google.maps.Marker({
-          map: map,
-          position: results[0].geometry.location
-        });
-      } else {
-        alert('Geocode was not successful for the following reason: ' + status);
-      }
-    });
-}
-
-
 document.addEventListener("DOMContentLoaded", function () {
   const recensioni = [
       { testo: "Servizio impeccabile! Puntuali, precisi e professionali. Consigliatissimo!", autore: "Marco R." },
